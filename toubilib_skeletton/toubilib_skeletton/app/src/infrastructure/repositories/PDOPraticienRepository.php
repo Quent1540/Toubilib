71f410f3a857751e87716fcdc7d3d4728098f591
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-09-09 16:43:02 +0200

    PDOPraticien

--- toubilib_skeletton/toubilib_skeletton/app/src/infrastructure/repositories/PDOPraticienRepository.php
+++ toubilib_skeletton/toubilib_skeletton/app/src/infrastructure/repositories/PDOPraticienRepository.php
@@ -1,17 +1,24 @@
 <?php
-
 namespace toubilib\infra\repositories;
 
-
+use toubilib\core\application\ports\spi\PraticienRepositoryInterface;
 
 class PDOPraticienRepository implements PraticienRepositoryInterface
 {
-
-
     private \PDO $pdo;
 
     public function __construct(\PDO $pdo) {
         $this->pdo = $pdo;
     }
- 
+
+    public function findAll(): array {
+        $stmt = $this->pdo->prepare('SELECT * FROM praticien');
+        $stmt->execute();
+
+        $praticiens = [];
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $praticiens[] = new \toubilib\core\domain\entities\praticien\Praticien($row['id'], $row['nom'], $row['prenom'], $row['specialite_id']);
+        }
+        return $praticiens;
+    }
 }
