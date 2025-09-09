7127a9b9b2831ca603526a4480c890058af232d5
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-09-09 14:16:29 +0200

    entité Praticien

--- toubilib_skeletton/toubilib_skeletton/app/src/application_core/domain/entities/praticien/Praticien.php
+++ toubilib_skeletton/toubilib_skeletton/app/src/application_core/domain/entities/praticien/Praticien.php
@@ -5,6 +5,31 @@ namespace toubilib\core\domain\entities\praticien;
 
 class Praticien
 {
- 
+    private string $id;
+    private string $nom;
+    private string $prenom;
+    private string $specialite_id;
 
+    public function __construct(string $id, string $nom, string $prenom, string $specialite_id){
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->specialite_id = $specialite_id;
+    }
+
+    public function getPratId(): string{
+        return $this->id;
+    }
+
+    public function getPratNom(): string{
+        return $this->nom;
+    }
+
+    public function getPratPrenom(): string{
+        return $this->prenom;
+    }
+
+    public function getPratSpecialiteId(): string{
+        return $this->specialite_id;
+    }
 }
