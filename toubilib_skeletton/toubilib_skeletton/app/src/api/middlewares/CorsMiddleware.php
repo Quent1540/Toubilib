a61f82a953eead0e710b738a8ab1ea43a7d35fb1
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-10-07 15:49:23 +0200

    gestion des headers CORS

---
+++ toubilib_skeletton/toubilib_skeletton/app/src/api/middlewares/CorsMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+namespace toubilib\api\middlewares;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+//Pour la gestion des headers CORS
+class CorsMiddleware implements MiddlewareInterface{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
